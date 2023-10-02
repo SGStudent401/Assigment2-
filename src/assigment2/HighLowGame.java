57c71c2a7997ad98b9891e79a4275312a86b20b2
SGStudent401 <grahamshelby401@gmail.com>
2023-10-02 12:49:14 -0500

    Update HighLowGame.java
    Changed chances from 1 to 0. Changed WHILE Loop conditions. Changed order of IF Statements.

--- src/assigment2/HighLowGame.java
+++ src/assigment2/HighLowGame.java
@@ -16,24 +16,16 @@ public class HighLowGame {
 		Scanner input = new Scanner(System.in);
 		//Declare variables
 		int randomNumber = random.nextInt(100) + 1;
-		int chances = 1;
+		int chances = 0;
 		//Prompt input from user
 		System.out.println("Pick a number between 1 and 100");
 		
 		//While loop conditions
-		while (chances <= 5) {
+		while (chances < 5) {
 			//Assign user's input to a variable
 			int guess = input.nextInt();
 			//IF statement for user guesses the random number
-			if (guess == randomNumber) {
-				System.out.println("You won!");
-				break;
-			}
-			//IF statement if user guesses 5 times
-			if (chances == 5) { 
-					System.out.println("You lose, the number to guess was: "+ randomNumber);
-					break;
-			}
+			
 			//IF statement if user guess outside the range
 			if (guess < 1 || guess > 100) {
 				System.out.println("Your guess is not between 1 and 100, please try again.\"");
@@ -43,11 +35,22 @@ public class HighLowGame {
 				System.out.println("Please pick a lower number");
 				chances++;
 			}
-			//Else IF statment if user's guess is too low
+			//Else IF statement if user's guess is too low
 			else if (guess < randomNumber && chances < 5)  {
 				System.out.println("Please pick a higher number");
 				chances++;
 			}
+			
+			if (guess == randomNumber) {
+				System.out.println("You won!");
+				break;
+			}
+			//IF statement if user guesses 5 times
+			if (chances == 5) { 
+					System.out.println("You lose, the number to guess was: "+ randomNumber);
+					break;
+			}
+			
 		}
 			
 	 }
