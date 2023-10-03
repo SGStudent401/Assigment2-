fa4132e237b6a8e2f89001a31b24003b8f8c299d
SGStudent401 <grahamshelby401@gmail.com>
2023-10-03 15:36:05 -0500

    Update HighLowGame.java
    Updated changes from video suggestions.

--- src/assigment2/HighLowGame.java
+++ src/assigment2/HighLowGame.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class HighLowGame {
-	
-	//This program is a High-Low Guessing Game
-	 //Game will loop for 5 guesses
-	 //Guesses outside the range do not count
 
+/**
+ * This program is a High-Low Guessing Game
+ * Game will loop for 5 guesses
+ * Guesses outside the range do not count
+ */
 	public static void main(String[] args) {
 		//Create a Random Number object
 		Random random = new Random();
@@ -24,36 +25,39 @@ public class HighLowGame {
 		while (chances < 5) {
 			//Assign user's input to a variable
 			int guess = input.nextInt();
+			
 			//IF statement for user guesses the random number
-			
-			//IF statement if user guess outside the range
-			if (guess < 1 || guess > 100) {
-				System.out.println("Your guess is not between 1 and 100, please try again.\"");
-			}
-			//Else IF statement if ueser's guess is too high
-			else if (guess > randomNumber && chances < 5) {
-				System.out.println("Please pick a lower number");
-				chances++;
-			}
-			//Else IF statement if user's guess is too low
-			else if (guess < randomNumber && chances < 5)  {
-				System.out.println("Please pick a higher number");
-				chances++;
-			}
-			
 			if (guess == randomNumber) {
 				System.out.println("You won!");
 				break;
 			}
+			//IF statement if user guess outside the range
+			else if (guess < 1 || guess > 100) {
+				System.out.println("Your guess is not between 1 and 100, please try again.\"");
+			}
+			//Else IF statement if ueser's guess is too high
+			else if (guess > randomNumber) {
+				chances++;
+				if (chances != 5) {
+				System.out.println("Please pick a lower number");
+				}
+			}
+			//Else IF statement if user's guess is too low
+			else if (guess < randomNumber)  {
+				chances++;
+				if (chances != 5) {
+				System.out.println("Please pick a higher number");
+				}
+			}
+		}
 			//IF statement if user guesses 5 times
 			if (chances == 5) { 
 					System.out.println("You lose, the number to guess was: "+ randomNumber);
-					break;
-			}
-			
+					
 		}
 			
 	 }
+	
 }
 	
 
